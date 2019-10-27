c60fe7912e62b9fed65b2f975bf80952f8e6ab26
cvweiss <cvweiss@gmail.com>
2019-10-27 20:30:15 +0000

    added limits

--- cron/fetch_locations.js
+++ cron/fetch_locations.js
@@ -7,10 +7,11 @@ async function f(app) {
     let rows = await app.db.information.find({
         'type': 'location_id',
         'last_updated': 0
-    }).toArray();
+    }).limit(100).toArray();
 
     for (let i = 0; i < rows.length; i++) {
         promises.push(fetch_locations(app, rows[i], rows[i].id));
+        await app.sleep(100);
     }
 
     await app.waitfor(promises);
@@ -19,13 +20,15 @@ async function f(app) {
 async function fetch_locations(app, row, solar_system_id) {
     console.log('Fetching fuzz map for system ' + solar_system_id);
     let res = await app.phin('https://www.fuzzwork.co.uk/api/mapdata.php?solarsystemid=' + solar_system_id + '&format=json');
-    let body = JSON.parse(res.body);
-    if (body.length > 0) {
-        await app.db.information.updateOne(row, {
-            $set: {
-                locations: body,
-                last_updated: 5570334857
-            }
-        });
+    if (res.statusCode == 200) {
+        let body = JSON.parse(res.body);
+        if (body.length > 0) {
+            await app.db.information.updateOne(row, {
+                $set: {
+                    locations: body,
+                    last_updated: 5570334857
+                }
+            });
+        }
     }
 }
