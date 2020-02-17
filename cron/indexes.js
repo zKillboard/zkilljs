926ec31dea25f36db75cffff58275d1c1c54dd15
cvweiss <cvweiss@gmail.com>
2020-02-17 03:06:02 +0000

    index update

--- cron/indexes.js
+++ cron/indexes.js
@@ -19,18 +19,18 @@ async function applyIndexes(app) {
         labels: 1
 
     };
-    await createIndex(app.db.killmails, index, bg);
+    await createIndex(app.db.collection('killmails'), index, bg);
     for (let i of ind) {
         var key = 'involved.' + i;
         index = {};
         index[key] = 1;
-        await createIndex(app.db.killmails, index, bg);
+        await createIndex(app.db.collection('killmails'), index, bg);
         
         index = {
             sequence: 1,
         };
         index[key] = 1;
-        await createIndex(app.db.killmails, index, bg);
+        await createIndex(app.db.collection('killmails'), index, bg);
         
         index = {
             stats: 1,
@@ -38,8 +38,24 @@ async function applyIndexes(app) {
         };
         var key = 'involved.' + i;
         index[key] = 1;
-        await createIndex(app.db.killmails, index, bg);
+        await createIndex(app.db.collection('killmails'), index, bg);
     }
+    await createIndex(app.db.collection('information'), {type: 1, id: 1}, {unique: true});
+    await createIndex(app.db.collection('information'), {last_updated: 1}, {});
+    await createIndex(app.db.collection('information'), {type: 1, last_updated: 1}, {});
+
+    await createIndex(app.db.collection('statistics'), {type: 1, id: 1}, {unique: true});
+    await createIndex(app.db.collection('statistics'), {update: 1}, {});
+
+    await createIndex(app.db.collection('killhashes'), {killmail_id: 1, hash: 1}, {unique: true});
+    await createIndex(app.db.collection('killhashes'), {status: 1}, bg);
+
+    await createIndex(app.db.collection('prices'), {item_id: 1}, {unique: true});
+    await createIndex(app.db.collection('prices'), {waiting: 1}, bg);
+
+    await createIndex(app.db.collection('rawmails'), {killmail_id: 1}, {unique: true});
+    await createIndex(app.db.collection('killmails'), {killmail_id: 1}, {unique: true});
+
     console.log('done');
 }
 
