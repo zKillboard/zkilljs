31bcbe87d443973d50ba11dc67178c9ecece7a84
cvweiss <cvweiss@gmail.com>
2020-04-14 00:44:13 +0000

    added more indexes

--- cron/indexes.js
+++ cron/indexes.js
@@ -14,11 +14,31 @@ async function f(app) {
 var bg = {};
 
 async function applyIndexes(app) {
+    await app.db.createCollection('killhashes');
+    await createIndex(app.db.killhashes, {killmail_id: 1, hash: 1}, {unique: true});
+    await createIndex(app.db.killhashes, {status: 1}, {});
+
+    await app.db.createCollection('statistics');
+    await createIndex(app.db.information, {type: 1, id: 1}, {unique: true});
+    await createIndex(app.db.information, {update: 1}, {});
+
+    await app.db.createCollection('prices');
+    await createIndex(app.db.prices, {item_id: 1}, {unique: true});
+    await createIndex(app.db.prices, {waiting: 1}, {});
+
+    await app.db.createCollection('information');
+    await createIndex(app.db.information, {type: 1, id: 1}, {unique: true});
+    await createIndex(app.db.information, {type: 1, last_updated: 1}, {});
+    await createIndex(app.db.information, {last_updated: 1}, {});
+
     var index = {
         sequence: 1,
         labels: 1
 
     };
+    await app.db.createCollection('killmails');
+    await createIndex(app.db.killmails, {killmail_id: 1}, {unique: true});
+    await createIndex(app.db.killmails, {sequence: 1}, {unique: true});
     await createIndex(app.db.killmails, index, bg);
     for (let i of ind) {
         var key = 'involved.' + i;
