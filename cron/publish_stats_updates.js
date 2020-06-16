5e98f0db644ac533b41d81514500ef4c7a68a362
cvweiss <cvweiss@gmail.com>
2020-06-16 18:33:58 +0000

    don't error on empty set

--- cron/publish_stats_updates.js
+++ cron/publish_stats_updates.js
@@ -1,20 +1,23 @@
 'use strict';
 
 async function f(app) {
-    while (true) {
-        if (app.bailout == true) return;
+    if (await app.redis.exists('zkilljs:stats:publish') > 0) {
+        await app.redis.rename('zkilljs:stats:publish', 'zkilljs:stats:publish_copy');
+        while (true) {
+            if (app.bailout == true) return;
 
-        var next = await app.redis.spop('zkilljs:stats:publish');
-        if (next == null) return;
+            var next = await app.redis.spop('zkilljs:stats:publish_copy');
+            if (next == null) break;
 
-        var json = JSON.parse(next);
+            var json = JSON.parse(next);
 
-        var base = '/' + json.type.replace('_id', '') + '/' + json.id;
-        var pubkey = 'statsfeed:' + base;
-        await app.redis.publish(pubkey, JSON.stringify({
-            action: 'statsfeed',
-            path: base
-        }));
+            var base = '/' + json.type.replace('_id', '') + '/' + json.id;
+            var pubkey = 'statsfeed:' + base;
+            await app.redis.publish(pubkey, JSON.stringify({
+                action: 'statsfeed',
+                path: base
+            }));
+        }
     }
 }
 
