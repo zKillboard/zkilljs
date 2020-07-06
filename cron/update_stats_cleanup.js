e705b43078ef357adb1b91d63bf5c63518f2e7ad
cvweiss <cvweiss@gmail.com>
2020-07-06 18:24:32 +0000

    reset expired week and recent stats

--- cron/update_stats_cleanup.js
+++ cron/update_stats_cleanup.js
@@ -1,16 +1,23 @@
 'use strict';
 
+var last_reset_check = 0;
+
 async function f(app) {
-    try {
-        if (app.bailout == true) return;
+    if (app.bailout == true) return;
 
-        var days7 = clear_kills(app, 'killmails_7', 'week', Math.floor(Date.now() / 1000) - (86400 * 7));
-        var days90 = clear_kills(app, 'killmails_90', 'recent', Math.floor(Date.now() / 1000) - (86400 * 90));
+    var days7 = clear_kills(app, 'killmails_7', 'week', Math.floor(Date.now() / 1000) - (86400 * 7));
+    var days90 = clear_kills(app, 'killmails_90', 'recent', Math.floor(Date.now() / 1000) - (86400 * 90));
 
-        await days7;
-        await days90;
-    } finally {
-        app.no_stats = false;
+    await days7;
+    await days90;
+
+    if ((Date.now() - last_reset_check) > 60000) {
+        // Find/reset any that have 0 kills or 0 losses
+        await app.db.statistics.updateMany({'recent.lost' : { $lte: 0}, 'recent.killed': {$lte: 0}}, {$set : { update_recent: true, 'recent.reset': true }}, {multi: true});
+        await app.db.statistics.updateMany({'recent.killed': {$lte: 0}}, {$set : { update_recent: true, 'recent.reset': true }}, {multi: true});
+        await app.db.statistics.updateMany({'week.lost' : { $lte: 0}, 'week.killed': {$lte: 0}}, {$set : { update_week: true, 'week.reset': true }}, {multi: true});
+        await app.db.statistics.updateMany({'week.killed': {$lte: 0}}, {$set : { update_week: true, 'week.reset': true }}, {multi: true});
+        last_reset_check = Date.now();
     }
 }
 
