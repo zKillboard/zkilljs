1669e985e1b9b39809df834806df0c23ca6f4f52
cvweiss <cvweiss@gmail.com>
2020-07-14 14:22:18 +0000

    ignoring market inflated prices for some items

--- util/price.js
+++ util/price.js
@@ -50,7 +50,12 @@ const price = {
         case 606:
             return 10000;
         case 12478: // Khumaak
+        case 34557: // barbican element
         case 34559: // Conflux Element
+        case 36902: // coalesced element
+        case 34560: // Redoubt element
+        case 34556: // sentinel element
+        case 34558: // vidette element
             return 0.01; // Items that get market manipulated and abused will go here
         case 44265: // Victory Firework
             return 0.01; // Items that drop from sites will go here
