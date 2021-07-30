db02603738d773e14725491616509eb68f5b5629
cvweiss <cvweiss@gmail.com>
2021-07-30 22:40:33 +0000

    create indexes without failing

--- cron/indexes.js
+++ cron/indexes.js
@@ -5,6 +5,25 @@ var first = true;
 
 async function f(app) {
     if (first) {
+
+        let success = false;
+        do {
+            try {
+
+                var collections = await app.db.listCollections().toArray();
+                for (let i = 0; i < collections.length; i++) {
+                    console.log('Prepping ' + collections[i].name);
+                    app.db[collections[i].name] = app.db.collection(collections[i].name);
+                }
+
+                await applyIndexes(app);
+                success = true;
+            } catch (e) {
+                await app.sleep(1000);
+                success = false;
+            }
+        } while (success == false);
+
         await applyIndexes(app);
         first = false;
     }
@@ -76,9 +95,44 @@ async function createIndex(collection, index, options) {
 }
 
 async function create_collection(app, collection) {
-    if (typeof app.db[collection] != 'undefined') return;
-    console.log('Creating collection', collection);
-    app.db[collection] = await app.db.createCollection(collection);
+    await app.db.createCollection(collection);
+
+    var index = {
+        sequence: 1,
+        labels: 1
+
+    };
+    await createIndex(app.db.collection(collection), index, {background: true});
+    var index = {
+        killmail_id: -1,
+        labels: 1
+
+    };
+    await createIndex(app.db.collection(collection), index, {background: true});
+        
+    await createIndex(app.db[collection], {padhash: 1}, {});
+    await createIndex(app.db[collection], {killmail_id: 1}, {unique: true});
+    await createIndex(app.db[collection], {sequence: 1}, {unique: true});
+    await createIndex(app.db[collection], index, bg);
+    for (let i of ind) {
+        var key = 'involved.' + i;
+        index = {};
+        index[key] = 1;
+        await createIndex(app.db.collection(collection), index, bg);
+        
+        index = {
+            stats: 1,
+            sequence: 1,
+        };
+        index[key] = 1;
+        await createIndex(app.db.collection(collection), index, bg);
+
+        index = {
+            killmail_id: -1
+        };
+        index[key] = 1;
+        await createIndex(app.db.collection(collection), index, bg);
+    }
 }
 
 //'labels'
