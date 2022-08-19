5a5b17dcb711db791034de85d6133566852f4cc1
cvweiss <cvweiss@gmail.com>
2022-08-19 10:48:38 -0400

    including maxage of 0 on redirects

--- www/controllers/site/kill-list.js
+++ www/controllers/site/kill-list.js
@@ -24,7 +24,7 @@ async function get(req, res) {
     req.alternativeUrl = '/cache/1hour/killmails/' + req.params.type + '/' + req.params.id + '.json';
     var valid = req.verify_query_params(req, valid);
     if (valid !== true) {
-        return {redirect: valid};
+        return {redirect: valid, maxAge: 0};
     }
 
     var page = Math.max(0, Math.min(9, req.query['page'])); // cannot go below 0 or above 9
