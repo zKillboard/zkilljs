767baca09f9c4f8e3ad2097031be5611016c8428
cvweiss <cvweiss@gmail.com>
2022-09-08 23:24:17 -0400

    quicker restart for excess memory usage

--- cron/gc.js
+++ cron/gc.js
@@ -9,12 +9,12 @@ let excessive_count = 0;
 
 async function f(app) {
 	let usage = Math.floor(process.memoryUsage().heapUsed / 1024 / 1024);
-	if (usage < 2048) {
+	if (usage < 1024) {
 		excessive_count = 0;
 	} else {
 		excessive_count++;
 	
-		if (excessive_count >= 20) {
+		if (excessive_count >= 30) {
 			await app.redis.setex("RESTART", 120, "true");
 			console.log('Excessive memory usage. Bailing for a reset.');
 		} else global.gc();
