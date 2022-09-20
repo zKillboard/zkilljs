38e7258aeb2a7082dbb3f50b06a25624dafba1d6
cvweiss <cvweiss@gmail.com>
2022-09-20 11:01:46 -0400

    handle non-existant type/id or extra slashes

--- www/controllers/site/index.js
+++ www/controllers/site/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-   paths: ['/', '/:type/:id'],
+   paths: ['/', '/:type/:id', '/:type/:id/'],
    get: get,
    priority: 1
 }
@@ -11,6 +11,13 @@ async function get(req, res) {
 
     let type = req.params.type;
     let id = req.params.id;
+
+    if (type == undefined) type = 'label';
+    if (id == undefined) id = 'pvp';
+
+    req.alternativeUrl = '/' + type + '/' + id;
+    let valid = req.verify_query_params(req, {});
+    if (valid !== true) return {redirect: valid};
     
     if (type != 'label') id = parseInt(id);
 
